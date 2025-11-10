18e60740afa4f1f6f74a0bd2309b0941f61e1ddc
mmarianaPinho <mariana.chimenes@estudante.ifms.edu.br>
2025-11-10 16:53:52 -0400

    comit do a3 feito

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,17 @@ import './index.css'
 import App from './App.tsx'
 import Carrinho from './Carrinho'
 import Login from './componentes/login/login.tsx'
+import ListaProdutos from './ListaProdutos.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/carrinho" element={<Carrinho />} />
+          <Route path="/" element={<App/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/carrinho" element={<Carrinho />} />
+          <Route path="/produtos" element={<ListaProdutos />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,

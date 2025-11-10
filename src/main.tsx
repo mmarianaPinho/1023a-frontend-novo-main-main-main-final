5b6ca8f7a09316076cd62b92280de4d9aee4b753
mmarianaPinho <mariana.chimenes@estudante.ifms.edu.br>
2025-11-10 15:57:32 -0400

    comit

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import Carrinho from './Carrinho'
 import Login from './componentes/login/login.tsx'
-import ListaProdutos from './Listarprodutos.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<App/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/carrinho" element={<Carrinho />} />
-          <Route path="/produtos" element={<ListaProdutos />} />
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/carrinho" element={<Carrinho />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
